41e4a23871e1d634b18324ae5ada63b482d157d8
Zinoks <borisenkovmaxim856@gmail.com>
2024-10-21 13:27:58 +0300

    Add SmartCaptcha

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,3 +1,4 @@
 <div class="container">
     <h2 class="mb-3">Добро пожаловать, <?php echo htmlspecialchars($_SESSION['user_name']); ?>!</h2>
+    <p>Доступ к этой странице есть только у вас.</p>
 </div>
--- src/login.php
+++ src/login.php
@@ -1,38 +1,85 @@
 <?php
-$email = '';
+    $login = '';
+    define('SMARTCAPTCHA_SERVER_KEY', ''); // Вставить токен
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = trim($_POST['email']);
-    $password = trim($_POST['password']);
+    function check_captcha($token)
+    {
+        $ch = curl_init();
 
-    $user = selectOne('users', ['email' => $email]);
+        $args = http_build_query([
+            "secret" => SMARTCAPTCHA_SERVER_KEY,
+            "token" => $token,
+            "ip" => $_SERVER['REMOTE_ADDR'],
+        ]);
 
-    if ($user && password_verify($password, $user['password'])) {
-        $_SESSION['user_name'] = $user['name'];
-        $_SESSION['user_id'] = $user['id'];
+        curl_setopt($ch, CURLOPT_URL, "https://smartcaptcha.yandexcloud.net/validate?$args");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 1);
 
-        header("Location: index.php?page=dashboard");
-        exit();
-    } else {
-        $errorMsg = 'Неверные email или пароль.';
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return $httpcode;
     }
-}
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $login = trim($_POST['login']);
+        $password = trim($_POST['password']);
+        $smartToken = isset($_POST['smart-token']) ? $_POST['smart-token'] : '';
+
+        if (!$smartToken) {
+            $errorMsg = 'Ошибка проверки капчи, попробуйте еще раз.';
+        } else {
+
+            $httpcode = check_captcha($smartToken);
+
+            if ($httpcode !== 0) {
+                $errorMsg = "Ошибка капчи, попробуйте еще раз.";
+            } else {
+                if (filter_var($login, FILTER_VALIDATE_EMAIL)) {
+                    $user = selectOne('users', ['email' => $login]);
+                } else {
+                    $user = selectOne('users', ['phone_number' => $login]);
+                }
+
+                if ($user && password_verify($password, $user['password'])) {
+                    $_SESSION['user_name'] = $user['name'];
+                    $_SESSION['user_id'] = $user['id'];
+
+                    header("Location: index.php?page=dashboard");
+                    exit();
+                } else {
+                    $errorMsg = 'Неверная почта/телефон или пароль.';
+                }
+            }
+        }
+    }
 ?>
 
 <div class="container">
     <h2 class="mb-3">Вход</h2>
-    <?php if (isset($errorMsg)): ?>
-        <div class="alert alert-danger"><?php echo $errorMsg; ?></div>
-    <?php endif; ?>
     <form action="" method="POST">
+        <?php if (isset($errorMsg)): ?>
+            <div class="alert alert-danger"><?php echo $errorMsg; ?></div>
+        <?php endif; ?>
         <div class="mb-3">
-            <label for="email" class="form-label">Почта или телефон</label>
-            <input type="text" class="form-control" id="email" name="email" required>
+            <label for="login" class="form-label">Почта или телефон</label>
+            <input type="text" class="form-control" id="login" name="login" required>
         </div>
         <div class="mb-3">
             <label for="password" class="form-label">Пароль</label>
             <input type="password" class="form-control" id="password" name="password" required>
         </div>
+
+        <div
+            style="height: 100px"
+            id="captcha-container"
+            class="smart-captcha mb-3"
+            data-sitekey="ysc1_QPsfIjQ8dpCaPODUN5dV3p58FvWBDoaQnS39fRkc7c3bf3e6"
+        ></div>
+
         <button type="submit" class="btn btn-primary">Войти</button>
     </form>
 </div>
+
+<script src="https://smartcaptcha.yandexcloud.net/captcha.js" defer></script>
